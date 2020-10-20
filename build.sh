1573820c05ab3d0e2f30932a65a8f1b7731c7ebd
Dan Fritchman <dan@fritch.mn>
2020-10-20 09:20:45 -0700

    Clean up, warnings etc

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ protoc -I=spice21/src --js_out=spice21js/spice21js spice21/src/*.proto
 # Sadly protoc doesn't seem to know how Python3 imports work 
 # https://github.com/protocolbuffers/protobuf/issues/1491
 protoc -I=spice21/src --python_out=spice21py/spice21py spice21/src/*.proto 
-sed -i -r 's/^import (.+_pb2.*)/from . import \1/g' spice21py/spice21py/*_pb2*.py
-
+sed -i -E 's/^import \(.*\)/from . import \1/g' spice21py/spice21py/*_pb2*.py
 
 # Publishing
 # cd spice21 && cargo publish
