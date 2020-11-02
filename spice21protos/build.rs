ca6ed040fc2fa7769e7e9afbcb4a88e359695eab
Dan Fritchman <dan@fritch.mn>
2020-11-01 19:44:58 -0800

    WIP Debug

--- spice21protos/build.rs
+++ spice21protos/build.rs
@@ -9,6 +9,6 @@ fn main() {
     println!("Spice21 build.rs");
     let mut config = prost_build::Config::new();
     // Add serde traits 
-    config.type_attribute(".", "#[derive(serde_derive::Serialize, serde_derive::Deserialize)]");
-    config.compile_protos(&["src/spice21.proto", "src/mos.proto", "src/bsim4.proto"], &["src/"]).unwrap();
+    // config.type_attribute(".", "#[derive(serde_derive::Serialize, serde_derive::Deserialize)]");
+    config.compile_protos(&["src/bsim4.proto"], &["src/"]).unwrap();
 }
