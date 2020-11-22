4a1b48129245cfd5261358b231838ef99693ff96
Dan Fritchman <dan@fritch.mn>
2020-11-22 15:45:55 -0800

    Initial JavaScript/ Node Bindings

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,20 @@
 
 # Rust core build
-# cd spice21 
-# cargo build 
-# cd .. 
+cd spice21 
+cargo build 
+cd .. 
 
-# Protobuf Compilation 
-protoc -I=spice21/src --js_out=spice21js/spice21js spice21/src/*.proto 
-
+# Python Protobuf Compilation 
 # Sadly protoc doesn't seem to know how Python3 imports work 
 # https://github.com/protocolbuffers/protobuf/issues/1491
 protoc -I=spice21/src --python_out=spice21py/spice21py spice21/src/*.proto 
 2to3 -wn -f import spice21py/spice21py/*pb2* 
+
+# JavaScript Protobuf Compilation 
+cd spice21js 
+yarn 
+yarn protoc 
+cd ..
 
 # Publishing
 # cd spice21 && cargo publish
